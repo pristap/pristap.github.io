4e494e9ee4107e44ae4a74da18a2a1f04b6a4abe
Jana Magdeska <jmagdeska@Janas-MacBook.local>
2015-08-18 18:13:45 +0200

    Responsive menu animation

--- js/menu-script.js
+++ js/menu-script.js
@@ -4,5 +4,6 @@ $(document).ready(function() {
     $('.page-content').toggleClass('hide-content');
     $('#content').toggleClass('hide-content');
     $('.site-footer').toggleClass('hide-content');
+    $('.responsive-menu').animate({top: '70px', opacity: '0.8'}, "slow");
   });
 });
