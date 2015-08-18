63c6663dd9060545eda08cbe8c285c1f38d45add
Jana Magdeska <jmagdeska@Janas-MacBook.local>
2015-08-18 16:00:20 +0200

    Responsive menu updated

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $('#menu-hidden').click(function() {
     $('.responsive-menu').toggleClass('expand');
+    $('.page-content').toggleClass('hide-content');
+    $('.site-footer').toggleClass('hide-content');
   });
 });
