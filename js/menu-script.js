34740bf49e77d06d4e1cfc66644bdf2914ffd6d5
Jana Magdeska <jmagdeska@Janas-MacBook.local>
2015-08-18 14:10:10 +0200

    Menu-hidden onclick event

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('#menu-hidden').click(function() {
-    $('.menu-background').css("width": "100%", "height": "90%", "background-color": "#BCC1C6");
+    $('.responsive-menu').toggleClass('expand');
   });
 });
