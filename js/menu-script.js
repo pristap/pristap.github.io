4919b978a7465174bf89a99b9f98d4c634b0db55
Jana Magdeska <jmagdeska@Janas-MacBook.local>
2015-08-19 13:31:22 +0200

    Responsive menu animation fixed

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   $('#menu-hidden').click(function() {
+    if( $('#nav-toggle').hasClass('active') ) {
+      $('.responsive-menu').animate({right: '0px', opacity: '0.9'}, "fast");
+    }
+    else {
+      $('.responsive-menu').animate({right: '-50px', opacity: '0.9'}, "fast");
+    }
     $('.responsive-menu').toggleClass('expand');
     $('.page-content').toggleClass('hide-content');
     $('#content').toggleClass('hide-content');
     $('.site-footer').toggleClass('hide-content');
-    $('.responsive-menu').animate({top: '70px', opacity: '0.8'}, "fast");
   });
 });
