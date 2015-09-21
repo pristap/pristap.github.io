6c82f830b9736c7f494278facf569875148c105d
Jana Magdeska <jmagdeska@Janas-MacBook.local>
2015-09-21 15:19:43 +0200

    Responsive menu updated

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
   $('#menu-hidden').click(function() {
-    if( $('#nav-toggle').hasClass('active') ) {
-      $('.responsive-menu').animate({right: '0px', opacity: '0.9'}, "fast");
-    }
-    else {
-      $('.responsive-menu').animate({right: '-50px', opacity: '0.9'}, "fast");
-    }
+    // if( $('#nav-toggle').hasClass('active') ) {
+    //   $('.responsive-menu').animate({right: '0px', opacity: '0.9'}, "fast");
+    // }
+    // else {
+    //   $('.responsive-menu').animate({right: '-50px', opacity: '0.9'}, "fast");
+    // }
+    // $('.responsive-menu').toggleClass('expand');
+    $('.responsive-menu').toggleClass('magictime slideUpRetourn');
     $('.responsive-menu').toggleClass('expand');
   });
 });
