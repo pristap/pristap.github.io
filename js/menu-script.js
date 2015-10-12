429ad9b92caf02799f9e624ef37f5ec793c8109f
Jana Magdeska <jmagdeska@Janas-MacBook.local>
2015-10-12 19:19:00 +0200

    Atom feed added & animations updated

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,13 +1,7 @@
 $(document).ready(function() {
+  $('.responsive-menu').removeClass('expand');
   $('#menu-hidden').click(function() {
-    // if( $('#nav-toggle').hasClass('active') ) {
-    //   $('.responsive-menu').animate({right: '0px', opacity: '0.9'}, "fast");
-    // }
-    // else {
-    //   $('.responsive-menu').animate({right: '-50px', opacity: '0.9'}, "fast");
-    // }
-    // $('.responsive-menu').toggleClass('expand');
-    $('.responsive-menu').toggleClass('magictime slideUpRetourn');
-    $('.responsive-menu').toggleClass('expand');
+    $('.responsive-menu').toggleClass('slideUpRetourn slideUp');
+    $('.responsive-menu').addClass('expand');
   });
 });
